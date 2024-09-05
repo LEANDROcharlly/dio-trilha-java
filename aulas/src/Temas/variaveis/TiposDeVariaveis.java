bd3d94ba3606d1822c299acb9d17a0110c9b0553
Leandro <leandro.alves1987@outlook.com>
2024-09-05 13:16:25 -0300

    inserido dados no arquivo java sobre variaveis e tipo

--- aulas/src/Temas/variaveis/TiposDeVariaveis.java
+++ aulas/src/Temas/variaveis/TiposDeVariaveis.java
@@ -1,5 +1,22 @@
 package Temas.variaveis;
 
 public class TiposDeVariaveis {
+    public static void main(String[] args){
+        //Tipos primitivos
+        String meuNome = "Leandro";
+        int idade = 37;
+        double salario = 4000.00;
+        Boolean casado = true;
+
+        System.out.println("nome: " + meuNome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Salario R$ " + salario);
+        System.out.println("Casado: " + casado);
+
+        //Constantes
+        final Double NUMERO_PI = 3.14;
+        System.out.println("Constante: " + NUMERO_PI);
+
+    }
 
 }
