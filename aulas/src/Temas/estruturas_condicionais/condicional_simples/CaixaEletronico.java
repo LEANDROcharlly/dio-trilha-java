1388a17a1b369378544f8ddded01b91288fa977a
LEANDROcharlly <106816645+LEANDROcharlly@users.noreply.github.com>
2024-09-09 22:29:16 -0300

    adicionado codigos nos diretorios referentes aos tipos de condicional

--- aulas/src/Temas/estruturas_condicionais/condicional_simples/CaixaEletronico.java
+++ aulas/src/Temas/estruturas_condicionais/condicional_simples/CaixaEletronico.java
@@ -2,6 +2,14 @@ package Temas.estruturas_condicionais.condicional_simples;
 
 public class CaixaEletronico {
     public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorsolicitado = 17.0;
+
+        if(valorsolicitado < saldo){
+            saldo = saldo - valorsolicitado;
+        }
+
+        System.out.println(saldo);
         
     }
 }
---
+++ aulas/src/Temas/estruturas_condicionais/switch_case/PlanoOperadora.java
@@ -0,0 +1,19 @@
+package Temas.estruturas_condicionais.switch_case;
+
+public class PlanoOperadora{
+    public static void main(String[] args){
+        String plano = "M";
+
+        switch (plano){
+            case "T":{
+                System.out.println("5Gb Youtube");
+            }
+            case "M":{
+                System.pout.println("Whats e Instagram grátis");
+            }
+            case "B":{
+                System.out.println()"100 minutos de ligação";
+            }
+        }
+    }
+}
