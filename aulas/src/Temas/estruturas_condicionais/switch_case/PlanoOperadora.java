48def88063fbc14f0bcc4787f3504d1c5da13201
Leandro <leandro.alves1987@outlook.com>
2024-09-11 14:22:27 -0300

    REALIZADO CORREÇÃO DE COMPATIBILIDADE

--- aulas/src/Temas/estruturas_condicionais/switch_case/PlanoOperadora.java
+++ aulas/src/Temas/estruturas_condicionais/switch_case/PlanoOperadora.java
@@ -9,10 +9,10 @@ public class PlanoOperadora{
                 System.out.println("5Gb Youtube");
             }
             case "M":{
-                System.pout.println("Whats e Instagram grátis");
+                System.out.println("Whats e Instagram grátis");
             }
             case "B":{
-                System.out.println()"100 minutos de ligação";
+                System.out.println("100 minutos de ligação");
             }
         }
     }
